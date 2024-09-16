2f1ae52f0f9aaec035c46b91bd4473e752a90747
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-15 18:58:19 -0700

    Update Dictionary from two lists.js

--- Dictionary from two lists.js
+++ Dictionary from two lists.js
@@ -21,3 +21,13 @@ Example 2:
 
 
 // Solution
+
+function createDict(keys, values) {
+  let obj = {}
+  
+  for (let i = 0; i < keys.length; i++) {
+    obj[keys[i]] = i < values.length ? values[i] : null
+  }
+  
+  return obj
+}
